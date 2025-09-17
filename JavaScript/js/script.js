e39deffa2da9c48a72db0004432ed67fb030df75
Nitin <nitinthakkar1992@gmail.com>
2025-09-17 07:23:46 +0530

    initial commit

---
+++ JavaScript/js/script.js
@@ -0,0 +1,32 @@
+function ArrayDemo() {
+            var student = ["Kashyp", "Dipesh", "Ankita", "Kishan"];
+            console.log(student[3]);
+
+        }
+
+        function whileLoopDemo() {
+            var i = 1;
+            while (i <= 10) {
+                console.log(i);
+                i=i+1;
+
+            }
+
+        }
+
+        function dowhileLoopDemo() {
+            var i = 1;
+           do {
+                console.log(i);
+                i=i+1;
+
+            } while (i <= 10);
+
+        }
+        function forLoopDemo() {
+          for(var i = 1; i<=10;i=i+1)
+          {
+            console.log(i);
+          }
+
+        }
